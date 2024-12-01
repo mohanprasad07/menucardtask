e432ef912599c7a7946f212d401c6bd48129e534
Sunil Prasad <mohanaprasadmit122@gst.sies.edu.in>
2024-12-02 02:18:32 +0530

    app.css

---
+++ new-portfolio/app.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>
+          This is where I showcase my work.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
